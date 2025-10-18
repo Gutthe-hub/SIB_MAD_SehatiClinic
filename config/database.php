6d8579c6710663916216c34c8fa91c694421462b
Gath-an <97538809+Nekophiliak@users.noreply.github.com>
2025-10-18 19:07:20 +0700

    commit pertama

--- config/database.php
+++ config/database.php
@@ -60,6 +60,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                // untuk fix error
+                PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
         ],
 
